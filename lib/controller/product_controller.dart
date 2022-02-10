86c1b739b0772706568e734c61ef77e43daa3f64
Rehan Hamayun <rr8329203@gmail.com>
2022-02-10 14:23:13 +0500

    first commit

---
+++ lib/controller/product_controller.dart
@@ -0,0 +1,57 @@
+import 'package:get/get.dart';
+
+import 'package:shopping_cart_getx/model/product_model.dart';
+
+class ProductController extends GetxController {
+  List<Product> productData = [];
+
+// Empty List used to store clicked add to cart values
+  List<Product> cartItem = [];
+
+// Add to cart Controller
+  addtoCart(Product item) {
+    cartItem.add(item);
+  }
+
+  // Total Amount Controller
+  double get totalPrice => cartItem.fold(0, (sum, item) => sum + item.price);
+  int get count => cartItem.length;
+
+  @override
+  void onInit() {
+    super.onInit();
+    fetchProductData();
+  }
+
+  fetchProductData() async {
+    await Future.delayed(Duration(seconds: 5));
+    List<Product> serverResponse = [
+      Product(
+        id: 1,
+        productName: "T-Shirt",
+        productImage: "assets/images/Blue_Tshirt.jpg",
+        productDescription: "This Shirt is cool as you",
+        price: 499,
+        favorite: false,
+      ),
+      Product(
+        id: 1,
+        productName: "Cotton Jeans",
+        productImage: "assets/images/jeans.png",
+        productDescription: "This Shirt is cool as you",
+        price: 100,
+        favorite: false,
+      ),
+      Product(
+        id: 1,
+        productName: "Snickers",
+        productImage: "assets/images/shoes.jpg",
+        productDescription: "This Shirt is cool as you",
+        price: 200,
+        favorite: false,
+      ),
+    ];
+    productData.assignAll(serverResponse);
+    //update();
+  }
+}
---
+++ lib/view/home_page.dart
@@ -0,0 +1,138 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:shopping_cart_getx/controller/product_controller.dart';
+
+class Homepage extends StatelessWidget {
+  final productController = Get.put(ProductController());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple,
+        elevation: 0,
+        title: Text("Products"),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 20.0),
+            child: ElevatedButton.icon(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.deepPurple,
+                  elevation: 0.0,
+                ),
+                onPressed: () {},
+                icon: Icon(Icons.shopping_cart),
+                label: Text(productController.count.toString())),
+          ),
+        ],
+      ),
+
+      // APP SCREEN
+      body: Column(
+        children: [
+          SizedBox(
+            height: 40,
+          ),
+          Text(
+            "Total Amount: ${productController.totalPrice.toString()} ",
+            style: TextStyle(
+                color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          Expanded(
+            child: GetBuilder<ProductController>(
+              builder: (controller) {
+                return ListView.builder(
+                    itemBuilder: (context, index) {
+                      return Container(
+                        child: Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Card(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(5),
+                            ),
+                            elevation: 0,
+                            child: Column(
+                              children: [
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Container(
+                                  height: 120,
+                                  width: double.infinity,
+                                  child: Image.asset(
+                                    productController
+                                        .productData[index].productImage,
+                                    fit: BoxFit.contain,
+                                  ),
+                                ),
+                                Container(
+                                  padding: EdgeInsets.all(5),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        productController
+                                            .productData[index].productName,
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 16),
+                                      ),
+                                      IconButton(
+                                        onPressed: () {},
+                                        icon: Icon(
+                                          productController
+                                                  .productData[index].favorite
+                                              ? Icons.favorite
+                                              : Icons.favorite_border,
+                                          color: Colors.red,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                Container(
+                                  padding: EdgeInsets.all(5),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        'Price : ${productController.productData[index].price} ',
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 16),
+                                      ),
+                                      ElevatedButton(
+                                        onPressed: () {
+                                          productController.addtoCart(
+                                              productController
+                                                  .productData[index]);
+                                        },
+                                        style: ElevatedButton.styleFrom(
+                                            primary: Colors.deepPurple),
+                                        child: Text(
+                                          'Add to Cart',
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 16),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                    itemCount: productController.productData.length);
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
